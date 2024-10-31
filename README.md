a008894b6eb8f536e9c1e51680bbb2c40874f8ae
Andre <andre.g2000@web.de>
2024-10-31 13:35:15 +0100

    Added a first implementation of DishGenerator. Performance is mediocre at max.

--- README.md
+++ README.md
@@ -6,3 +6,15 @@
 export GOOGLE_API_KEY="<KEY>"
 export GOOGLE_SEARCH_ENGINE_ID="<ID>"
 ```
+
+# DISH GENERATION
+Generate token for LLaMa on HuggingFace, request permission for meta-llama/Llama-3.2-1B
+If you want to use on your own device download it by using:
+```bash
+set HF_TOKEN=your_token_here
+huggingface-cli download meta-llama/Llama-3.2-1B --include "original/*" --local-dir /path/to/save/directory/Llama-3.2-1B
+```
+However if wanting to use Google Colab, please insert your HuggingFace token in
+```python
+login(token='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')
+```
---
+++ dish-generator/dish-generator.py
@@ -0,0 +1,38 @@
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+#from huggingface_hub import login
+#login(token='your-token-here')
+
+#Careful: Downloads the specified model into .cache if used locally.
+class DishGenerator:
+    def __init__(self, model_id="meta-llama/Llama-3.2-1B", torch_dtype=torch.bfloat16):
+        self.model_id = model_id
+        self.tokenizer = AutoTokenizer.from_pretrained(model_id)
+        if self.tokenizer.pad_token is None:
+            self.tokenizer.pad_token = self.tokenizer.eos_token
+            
+        self.model = AutoModelForCausalLM.from_pretrained(model_id)
+
+    def generate_dish_description(self, ingredients, max_length=200):
+        ingredients_string = ", ".join(ingredients) 
+        #prompt = "Tell me an existing dish and only the dish that includes these ingredients: " + ingredients_string
+        prompt = "I have these ingredients: "+ ingredients_string +". A possible dish of these ingredients could be:"
+        # Tokenize the input prompt and specify padding options
+        inputs = self.tokenizer(prompt, return_tensors="pt", padding=True)
+        
+        # Set the pad_token_id to eos_token_id for handling padding
+        outputs = self.model.generate(
+            inputs['input_ids'], 
+            attention_mask=inputs['attention_mask'],  # Pass the attention mask here
+            max_length=max_length, 
+            pad_token_id=self.tokenizer.eos_token_id  # Set pad_token_id to eos_token_id
+        )
+        return self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+#Testing
+#On Windows the model is safed in C:\Users\<YourUsername>\.cache\huggingface\transformers\
+
+#dish_gen = DishGenerator()
+#ingredients = ["banana", "milk"]
+#dish_description = dish_gen.generate_dish_description(ingredients)
+#print(dish_description)
