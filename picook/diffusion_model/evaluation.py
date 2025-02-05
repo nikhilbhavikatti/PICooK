db91939fad4fcaccd4d64e1049c2347b8d3c00ad
Jan <j.hagnberger@gmx.de>
2025-02-05 18:27:49 +0100

    minor changes

--- picook/diffusion_model/evaluation.py
+++ picook/diffusion_model/evaluation.py
@@ -13,6 +13,7 @@ from functools import reduce
 
 from torchmetrics.image.fid import FrechetInceptionDistance
 from torchmetrics.functional.multimodal import clip_score
+from torchmetrics.multimodal.clip_score import CLIPScore
 
 from diffusion_pipeline import PicookDiffusionPipeline
 
@@ -121,7 +122,7 @@ def compute_fid():
     mean_clip_score = 0
     good_images = 0
 
-    clip_score_fn = partial(clip_score, model_name_or_path="openai/clip-vit-base-patch16")
+    clip_score_fn = partial(clip_score, model_name_or_path="openai/clip-vit-base-patch32")
 
     def calculate_clip_score(images, prompts):
         clip_score = clip_score_fn(torch.from_numpy(images_int).permute(0, 3, 1, 2), prompts).detach()
@@ -130,6 +131,7 @@ def compute_fid():
     generator = torch.Generator(device=device)
     generator = generator.manual_seed(seed)
 
+    clip_score_metric = CLIPScore(model_name_or_path="openai/clip-vit-base-patch32")
 
     with torch.no_grad():
         for batch in tqdm(test_dataloader):
@@ -150,7 +152,7 @@ def compute_fid():
             images = pipeline(encoder_hidden_states=encoder_hidden_states, attention_mask=attention_mask,
                                 batch_size=batch_size,
                                 num_inference_steps=30,
-                                generator=generator, is_training=False, output_type="numpy").images
+                                is_training=False, output_type="numpy").images
 
             # Get real images (denormalize)
             dishes = batch["pixel_values"]
@@ -163,11 +165,13 @@ def compute_fid():
 
 
             # clip score
-            score = clip_score_fn(images, ["a photo of something to eat like food or a cooked dish"] * batch_size).detach()
+            score = clip_score_fn(dishes, ["a photo of a dish"] * batch_size).detach()
+            print(score)
             mean_clip_score += score * batch_size
             if score > 0.75:
                 good_images += 1 * batch_size
             
+            clip_score_metric.update(dishes, ["a photo of a dish"] * batch_size)
             # clear memory  
             torch.cuda.empty_cache()
 
@@ -175,6 +179,9 @@ def compute_fid():
 
     fid = fid.compute()
     print(f"FID Score:", fid)
+
+    num = clip_score_metric.compute()
+    print(f"CLIP Score:", num)
 
     good_fraction = good_images / len(test_dataset)
     print(f"Good images fraction:", good_fraction)
@@ -226,11 +233,13 @@ def plot_images_with_conditioning():
             attention_mask = attention_mask.to(device)
 
             # run pipeline
+            generator = torch.Generator(device=device)
+            generator = generator.manual_seed(69)
             images = pipeline(encoder_hidden_states=encoder_hidden_states, attention_mask=attention_mask,
                                     batch_size=batch_size,
                                     num_inference_steps=30,
                                     guidance_scale=1.0,
-                                    is_training=False, output_type="numpy").images
+                                    generator=generator, is_training=False, output_type="numpy").images
             images = (torch.tensor(images) * 255).clamp(0, 255).to(torch.uint8)
             img.append(images)
             im = Image.fromarray(images[0].cpu().numpy())
@@ -269,6 +278,97 @@ def plot_images_with_conditioning():
     plt.savefig("input_output.png")
 
 
+
+def plot_images_with_removed_conditioning():
+    # run inference
+    # Get the image embedding for conditioning
+    
+    base_path = "../../data/dishes_dataset/test"
+    #selection = [10, 1532, 1222, 2747] for paper
+    selection = [1222]
+    max_sequence_length = 20
+    img = []
+    ing = []
+    dishes = []
+
+    for idx in selection:
+        for j in range(0,8):
+            path = os.path.join(base_path, str(idx))
+            ingredients = glob.glob(os.path.join(path, "ingredient*.jpg"))
+            if j > 0:
+                ingredients = ingredients[:-j]
+            dish = glob.glob(os.path.join(path, "dish.jpg"))
+            num_ingredients = torch.tensor([len(ingredients)])
+
+            if len(ingredients) == 0 or len(dish) == 0:
+                raise ValueError("No ingredients or dish found")
+
+            ingredients = [Image.open(img).convert("RGB") for img in ingredients]
+            ing.append([resize(ingredient_img) for ingredient_img in ingredients])
+            ingredients = x_transform(images=ingredients, return_tensors="pt")["pixel_values"]
+            padding = torch.zeros(max_sequence_length - num_ingredients, 3, 224, 224)
+            ingredients = torch.concat((ingredients, padding), dim=0)
+            dishes.append(resize(Image.open(dish[0]).convert("RGB")))
+
+            with torch.no_grad():
+                batch_size = 1
+
+                inp = ingredients.unsqueeze(0).to(device, dtype=weight_dtype)
+                inp = rearrange(inp, "b n c h w -> (b n) c h w")
+                outputs = image_model(inp)
+                last_hidden_state = outputs.last_hidden_state
+                pooled_output = outputs.pooler_output
+                encoder_hidden_states = rearrange(pooled_output, "(b n) c -> b n c", b=batch_size)
+
+                # attention mask for unet
+                attention_mask = (torch.arange(20)[None, :].repeat(batch_size, 1) < num_ingredients[:, None]).int()
+                attention_mask = attention_mask.to(device)
+                print(attention_mask)
+
+                # run pipeline
+                generator = torch.Generator(device=device)
+                generator = generator.manual_seed(1234)
+                #generator = generator.manual_seed(5467)
+                images = pipeline(encoder_hidden_states=encoder_hidden_states, attention_mask=attention_mask,
+                                        batch_size=batch_size,
+                                        num_inference_steps=30,
+                                        guidance_scale=1.0,
+                                        generator=generator, is_training=False, output_type="numpy").images
+                images = (torch.tensor(images) * 255).clamp(0, 255).to(torch.uint8)
+                img.append(images)
+                im = Image.fromarray(images[0].cpu().numpy())
+                im.save(f"generated_image_{idx}.jpeg")
+
+    images = np.concatenate(img, axis=0)
+
+    # Create a figure with specified dimensions
+    #fig, axes = plt.subplots(1, 4, figsize=(24, 13))
+    max_seq_len = reduce(lambda x, y: max(x, y), [len(x) for x in ing])
+    fig, axes = plt.subplots(len(selection * 8), max_seq_len+2, figsize=(18, 20), gridspec_kw={'width_ratios': [1] * (max_seq_len) + [0.3, 1]})
+
+    # Plot each image in the respective subplot
+    for i in range(len(selection) * 8):
+        # ingredients
+        for j, ingredient in enumerate(ing[i]):
+            axes[i, j].imshow(ingredient)
+
+        # pred
+        axes[i, -1].imshow(images[i, :, :, :])
+        
+    # title
+    axes[0,-1].set_title("Generated\nImage", fontsize=20)
+
+    # hide all axis
+    for ax in axes.flatten():
+        ax.axis('off')  # Hide the axis
+
+    # Adjust layout
+    plt.tight_layout()
+    plt.savefig("input_output_cond_removed.pdf")
+    plt.savefig("input_output_cond_removed.png")
+
+
 #plot_images()
 #compute_fid()
-plot_images_with_conditioning()
+#plot_images_with_conditioning()
+plot_images_with_removed_conditioning()
